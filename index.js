1c832184d951223673badd9a730e17983c074f3f
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-04 15:40:22 -0600

    Add function to add and get requests

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const shortid = require('shortid');
+const server = express();
+
+server.use(express.json());
+
+let users = [
+    {
+        id:shortid.generate(),
+        name: 'John Dale',
+        bio: 'Simple Farmer'
+    }
+]
+
+server.get('/', (req, res) =>{
+    res.json({api:'Up and Running!'});
+})
+
+server.get('/api/users', (req, res) => {
+
+    res.json(users);
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    let certainUser = users.filter(person => person.id === id);
+
+    res.status(200).json(certainUser);
+})
+
+server.post('/api/users', (req, res) => {
+    let userInfo = req.body;
+    userInfo.id = shortid.generate();
+
+
+    if(userInfo.name === null || userInfo.bio === null || userInfo.name === "" || userInfo.bio === ""){
+        res.status(400).json({Error:'Please Provide Name and Bio for the user'})
+    }
+    else if(!userInfo){
+        res.status(400).json({Error:'There was an error trying to get the data.'});
+    }
+    else{
+        users.push(userInfo);
+        res.status(201).json(users);
+    }
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    let id = req.params.id;
+
+    users = users.filter(person => person.id != id);
+
+    res.status(200).json(users);
+})
+
+server.patch('/api/user/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    res.status(200).json(users);
+})
+
+server.listen(5000, () => console.log('API is Running!'));
